49e19a5fa846117301e00ff046ce3adf3c145666
Skullbonk <reagan.fairbanks@gmail.com>
2018-10-09 13:46:17 -0600

    Made the app

--- FirstRealStupidApp/Controller/ColorChangerViewController.swift
+++ FirstRealStupidApp/Controller/ColorChangerViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 public class ColorChangerViewController: UIViewController
 {
-
+    private lazy var myColor : ColorTool = ColorTool()
+    
+    @IBOutlet weak var FirstButton: UIButton! // ! means this is nil, touch it and you die
+    
     public override func viewDidLoad() -> Void
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
-
+   
+    @IBAction func colorClick(_ sender: UIButton)
+    {
+        self.view.backgroundColor = myColor.createRandomColor()
+        FirstButton.backgroundColor = myColor.createRandomColor()
+    }
 }
+    
+
+
 
 
 
