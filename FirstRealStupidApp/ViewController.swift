fe64b5fdfbcf085bbe6798b0aa90147dbda555d7
Skullbonk <reagan.fairbanks@gmail.com>
2018-10-09 11:56:04 -0600

    Initial Commade gitigntnorenosdafmit

---
+++ FirstRealStupidApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstRealStupidApp
+//
+//  Created by Fairbanks, Reagan on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
